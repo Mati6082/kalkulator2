8bff44f15868009023d2d95ea828740d93bcd08c
matpakla <matpakla@gmail.com>
2023-09-25 20:37:44 +0200

    xD

--- app/src/main/java/com/example/kalkulator2/MainActivity.kt
+++ app/src/main/java/com/example/kalkulator2/MainActivity.kt
@@ -4,14 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     lateinit var wysw: TextView
-    var obecne: Double? = null
-    var poprzednie: Double? = null
+    var ob: Double? = null
+    var pop: Double? = null
     var dzialanie: String? = null
+    var switch = false
+    lateinit var pom: String
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,53 +22,186 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun click(v: View) {
-        var press: String = ""
-        var pom: Double
+        if (dzialanie == null) pom = wysw.text.toString()
+        else pom = "0"
+        wysw.text = ""
+        var press = ""
         when (v.id) {
-            R.id.btn0 -> press = "0"
-            R.id.btn1 -> press = "1"
-            R.id.btn2 -> press = "2"
-            R.id.btn3 -> press = "3"
-            R.id.btn4 -> press = "4"
-            R.id.btn5 -> press = "5"
-            R.id.btn6 -> press = "6"
-            R.id.btn7 -> press = "7"
-            R.id.btn8 -> press = "8"
-            R.id.btn9 -> press = "9"
-            R.id.btnPlus -> dzialanie = "+"
+            R.id.btn0 -> {
+                if (switch) {
+                    wysw.text = "0"
+                    switch = false
+                    pom = ""
+                }
+                press = "0"
+            }
+
+            R.id.btn1 -> {
+                if (switch) {
+                    wysw.text = "1"
+                    switch = false
+                    pom = ""
+                }
+                press = "1"
+            }
+
+            R.id.btn2 -> {
+                if (switch) {
+                    wysw.text = "2"
+                    switch = false
+                    pom = ""
+                }
+                press = "2"
+            }
+
+            R.id.btn3 -> {
+                if (switch) {
+                    wysw.text = "3"
+                    switch = false
+                    pom = ""
+                }
+                press = "3"
+            }
+
+            R.id.btn4 -> {
+                if (switch) {
+                    wysw.text = "4"
+                    switch = false
+                    pom = ""
+                }
+                press = "4"
+            }
+
+            R.id.btn5 -> {
+                if (switch) {
+                    wysw.text = "5"
+                    switch = false
+                    pom = ""
+                }
+                press = "5"
+            }
+
+            R.id.btn6 -> {
+                if (switch) {
+                    wysw.text = "6"
+                    switch = false
+                    pom = ""
+                }
+                press = "6"
+            }
+
+            R.id.btn7 -> {
+                if (switch) {
+                    wysw.text = "7"
+                    switch = false
+                    pom = ""
+                }
+                press = "7"
+            }
+
+            R.id.btn8 -> {
+                if (switch) {
+                    wysw.text = "8"
+                    switch = false
+                    pom = ""
+                }
+                press = "8"
+            }
+
+            R.id.btn9 -> {
+                if (switch) {
+                    wysw.text = "9"
+                    switch = false
+                    pom = ""
+                }
+                press = "9"
+            }
+
+            R.id.btnPlus -> {
+                if (dzialanie != "+") {
+                    dzialanie = "+"
+                    press = ""
+                } else {
+                    press = wykonajDzialanie(dzialanie!!, ob!!, pop!!).toString()
+                    dzialanie = null
+                    switch = true
+                }
+            }
+
+            R.id.btnMinus -> {
+                if (dzialanie != "-") {
+                    dzialanie = "-"
+                    press = ""
+                } else {
+                    press = wykonajDzialanie(dzialanie!!, ob!!, pop!!).toString()
+                    dzialanie = null
+                    switch = true
+                }
+            }
+
+            R.id.btnDivide -> {
+                if (ob != 0.0) {
+                    if (dzialanie != "/") {
+                        dzialanie = "/"
+                        press = ""
+                    } else {
+                        press = wykonajDzialanie(dzialanie!!, ob!!, pop!!).toString()
+                        dzialanie = null
+                        switch = true
+                    }
+                } else press = "0"
+            }
+
+            R.id.btnMultiply -> {
+                if (dzialanie != "*") {
+                    dzialanie = "*"
+                    press = ""
+                } else {
+                    press = wykonajDzialanie(dzialanie!!, ob!!, pop!!).toString()
+                    dzialanie = null
+                    switch = true
+                }
+            }
+
             R.id.btnEquals -> {
-                wysw.text = wykonajDzialanie(dzialanie!!, obecne!!, poprzednie!!).toString()
-                dzialanie = null
+                if (dzialanie != null && ob != null && pop != null) {
+                    press = wykonajDzialanie(dzialanie!!, ob!!, pop!!).toString()
+                    dzialanie = null
+                    switch = true
+                } else press = "0"
             }
         }
         if (dzialanie == null) {
-            if (wysw.text.toString() == "0") {
-                poprzednie = 0.0
-                obecne = press.toDouble()
+            if (pom == "0") {
                 wysw.text = press
             } else {
-                poprzednie = wysw.text.toString().toDouble()
-                wysw.text = wysw.text.toString() + press
-                obecne = wysw.text.toString().toDouble()
-
+                wysw.text = pom + press
             }
+            pop = wysw.text.toString().toDouble()
+        } else {
+            if (pom == "0" && press != "") {
+                wysw.text = press
+            } else if (press != "") {
+                wysw.text = pom + press
+            } else wysw.text = pop.toString()
+            ob = wysw.text.toString().toDouble()
         }
     }
 
     fun wykonajDzialanie(dzialanie: String, obecna: Double, poprzednia: Double): Double? {
         when (dzialanie) {
-            "+" -> return obecna + poprzednia
-            "*" -> return obecna * poprzednia
-            "/" -> return obecna / poprzednia
-            "-" -> return obecna - poprzednia
+            "+" -> return poprzednia + obecna
+            "*" -> return poprzednia * obecna
+            "/" -> return poprzednia / obecna
+            "-" -> return poprzednia - obecna
         }
         return null
     }
 
-
     fun clear(v: View) {
         wysw.text = "0"
-        obecne = null
-        poprzednie = null
+        ob = null
+        pop = null
+        dzialanie = null
     }
 }
